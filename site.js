5b161314102252fcadd196d7413cc93641f6d4a0
emmakoz15 <emmakozlovsky@gmail.com>
2024-12-12 14:17:53 -0500

    Steps 1-4

--- site.js
+++ site.js
@@ -34,6 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title:"IMDB Emmma's Top 8 Movies",
+            owner:"Emma",
+            github:"https://github.com/emmakoz15/NJIT-3_StarterFiles"
          
       }
     },
